1c73e10f163e4e4adbba1d822be338a1c038bc64
Sree Lakshmi <sinisreelaksmi19@gmail.com>
2025-03-03 23:35:16 +0530

    generate otp

---
+++ script.js
@@ -0,0 +1,15 @@
+const otpfield = document.getElementById("otp");
+const btn = document.getElementById("btn");
+
+function getOTP() {
+    let otp = "";
+    for (let i = 0; i < 6; i++) {
+        otp = otp + Math.floor(Math.random() * 10);
+    }
+    return otp;
+}
+
+btn.addEventListener('click', () => {
+    let otp = getOTP();
+    otpfield.value = otp;
+})
