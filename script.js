794d2b71e7ab2363ca0027e209cb26ac7fc9a9f0
Sree Lakshmi <sinisreelaksmi19@gmail.com>
2025-03-20 20:52:55 +0530

    pop up

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const otpfield = document.getElementById("otp");
 const btn = document.getElementById("btn");
 const copy = document.getElementById("copy");
+const msg = document.getElementById("msg");
+const msgContainer = document.getElementsByClassName("msgContainer");
 
 function getOTP() {
     let otp = "";
     for (let i = 0; i < 6; i++) {
-        otp = otp + Math.floor(Math.random() * 10);
+        otp += Math.floor(Math.random() * 10);
     }
     return otp;
 }
@@ -13,8 +15,18 @@ function getOTP() {
 btn.addEventListener('click', () => {
     let otp = getOTP();
     otpfield.value = otp;
+    msgContainer.style.display = "block";
+    msg.innerText = "OTP Generated!";
+    setTimeout(() => {
+        msgContainer.style.display = "none";
+    }, 2000);
 });
 
 copy.addEventListener('click', () => {
     navigator.clipboard.writeText(otpfield.value);
-})
+    msgContainer.style.display = "block";
+    msg.innerText = "OTP copied to clipboard!";
+    setTimeout(() => {
+        msgContainer.style.display = "none";
+    }, 2000);
+});
